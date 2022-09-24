7c446968ea58492a71dd22e7ea84ac867fa0517e
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-24 18:36:40 +0200

    more robust and cleaner implementation

--- main.js
+++ main.js
@@ -19,11 +19,7 @@ export class App {
   async changeColor(color) {
     if (color[0] == "#") color = color.substring(1);
     if (color.length != 6) return false;
-    if (!this.client) await this.findClient();
-    if (!this.client) return "Can't find client in reach of device.";
-    await this.client.request("ble", "connect", [this.config.address]);
-    await this.client.request("ble", "discover_services", []);
-    await this.client.request("ble", "write_to_uuid", [
+    let result = await this.writeToClient([
       0xffd9,
       [
         0x56,
@@ -35,31 +31,21 @@ export class App {
         0xaa,
       ],
     ]);
+
+    if (result) return result;
   }
 
   async turnOn() {
-    if (!this.client) await this.findClient();
-    if (!this.client) return "Can't find client in reach of device.";
-    await this.client.request("ble", "connect", [this.config.address]);
-    await this.client.request("ble", "discover_services", []);
-    await this.client.request("ble", "write_to_uuid", [
-      0xffd9,
-      [0xcc, 0x23, 0x33],
-    ]);
+    let result = await this.writeToClient([0xffd9, [0xcc, 0x23, 0x33]]);
 
+    if (result) return result;
     this.on = true;
   }
 
   async turnOff() {
-    if (!this.client) await this.findClient();
-    if (!this.client) return "Can't find client in reach of device.";
-    await this.client.request("ble", "connect", [this.config.address]);
-    await this.client.request("ble", "discover_services", []);
-    await this.client.request("ble", "write_to_uuid", [
-      0xffd9,
-      [0xcc, 0x24, 0x33],
-    ]);
+    let result = await this.writeToClient([0xffd9, [0xcc, 0x24, 0x33]]);
 
+    if (result) return result;
     this.on = false;
   }
 
@@ -76,6 +62,18 @@ export class App {
           break clientLoop;
         }
       }
+    }
+  }
+
+  async writeToClient(write) {
+    if (!this.client) await this.findClient();
+    if (!this.client) return "Can't find client in reach of device.";
+    await this.client.request("ble", "connect", [this.config.address]);
+    await this.client.request("ble", "discover_services", []);
+    let writeResult = await this.client.request("ble", "write_to_uuid", write);
+    if (!writeResult || !writeResult.result) {
+      this.client = undefined;
+      return await this.writeToClient(write);
     }
   }
 
